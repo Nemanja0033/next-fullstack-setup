529dd2c098a850dc8fab5eb37f9bc7115a06ff7c
Nemanja Antonijevic <antonijevicnemanja68@gmail.com>
2025-06-22 12:23:36 +0200

    setting auth function

---
+++ lib/auth/auth.ts
@@ -0,0 +1,45 @@
+import {
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithEmailAndPassword,
+    signOut,
+    signInWithPopup
+  } from 'firebase/auth'
+  import { auth } from './firebaseConfig'
+  
+  export async function registerUser(email: string, password: string) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      return userCredential.user
+    } catch (error: any) {
+      throw new Error(error.message)
+    }
+  }
+  
+  export async function loginUser(email: string, password: string) {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      return userCredential.user
+    } catch (error: any) {
+      throw new Error(error.message)
+    }
+  }
+  
+  export async function logoutUser() {
+    try {
+      await signOut(auth)
+    } catch (error: any) {
+      throw new Error(error.message)
+    }
+  }
+
+  export async function singInWithGoogle(){
+    try{
+        const provider = new GoogleAuthProvider();
+        const result = await signInWithPopup(auth, provider);
+        return result.user; 
+    }
+    catch (error: any) {
+        throw new Error(error.message);
+    }
+  }  
